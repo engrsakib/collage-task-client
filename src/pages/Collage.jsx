25b19c0c600c8f208a7dc0fd80c4b0d8a8151322
engrsakib <snazmussakib01@gmail.com>
2025-03-03 12:03:14 +0600

    collage axios added

--- src/pages/Collage.jsx
+++ src/pages/Collage.jsx
@@ -1,11 +1,45 @@
-import React from 'react';
+import React, { useContext } from "react";
+import { AuthContext } from "../provider/AuthProvider";
+import useGetAllUsers from "../components/Dashboard/user/AllUsers/useGetAllUsers";
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+import { Helmet } from "react-helmet";
+
 
 const Collage = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+  const { setdark, dark, user } = useContext(AuthContext);
+  const { users } = useGetAllUsers(user);
+
+  const {
+    isLoading: isPending,
+    data: College = [],
+    refetch,
+  } = useQuery({
+    queryKey: ["collage"],
+    queryFn: async () => {
+      try {
+        const response = await axios.get(
+          "http://localhost:5000/university"
+        );
+        return response.data;
+      } catch (error) {
+        console.error("Error fetching blogs:", error);
+        return [];
+      }
+    },
+  });
+
+  console.log(College)
+
+  return <>
+
+
+
+<Helmet>
+    <meta charSet="utf-8" />
+    <title>Colleges</title>
+</Helmet>
+  </>;
 };
 
 export default Collage;
